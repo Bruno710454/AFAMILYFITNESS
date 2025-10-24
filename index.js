c98d282ba5001de611f69bb42d5ad24d40a612a3
Bruno Mikael <brunomikael@outlook.com.br>
2025-10-24 16:00:37 -0300

    defsdf
    sdfsdf

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 var cc = 0 
 
 function mostrar(){
+    var img = document.getElementById("img")
+    var img2 = document.getElementById("img2")
     var head = document.getElementById("head")
     cc++
+    img2.style.display="none"
+    img.style.display="block"
     if (cc==1){
         head.style.height="200px"
         head.style.transition="0.5s"
@@ -12,6 +16,8 @@ function mostrar(){
         head.style.height="0px"
         head.style.transition="0.5s"
         cc=0
+         img2.style.display="block"
+        img.style.display="none"
 
     }
 
